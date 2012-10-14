53edcdb13a45e172055e94a8ebd2d15575d414ab
John Hutchinson <johnhutch@gmail.com>
2012-10-14 19:25:53 -0400

    got the set union of a night's games displaying

--- spec/requests/nights_spec.rb
+++ spec/requests/nights_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 describe "Nights" do
+    let(:night) {FactoryGirl.create(:night)}
     let(:user) {FactoryGirl.create(:user)}
+    let(:user2) {FactoryGirl.create(:user)}
+    let(:user3) {FactoryGirl.create(:user)}
+    let(:user4) {FactoryGirl.create(:user)}
+    let(:game) {FactoryGirl.create(:game)}
+    let(:game2) {FactoryGirl.create(:game)}
+    let(:game3) {FactoryGirl.create(:game)}
+    let(:game4) {FactoryGirl.create(:game)}
+    let(:game5) {FactoryGirl.create(:game)}
+    let(:game6) {FactoryGirl.create(:game)}
+    let(:game7) {FactoryGirl.create(:game)}
+    let(:game8) {FactoryGirl.create(:game)}
 
     describe "POST /nights" do
         it "gamer user can create a game night" do
@@ -15,6 +27,9 @@ describe "Nights" do
         end
 
         it "gamer can delete a game night" do
+            user
+            user2
+            user3
             login(user)
             user.nights.create!
 
@@ -22,6 +37,33 @@ describe "Nights" do
             click_link I18n.t('links.remove_night')
             page.should have_content(I18n.t('flash.night_removed'))
             page.should have_content("You have no game nights set up.")
+        end
+    end
+
+    describe "SHOW /night/:id" do
+        it "shows all of the games owned by the night across all users" do
+            user.games << game
+            user.games << game2
+            user.games << game3
+            user2.games << game3
+            user2.games << game4
+            user2.games << game5
+            user3.games << game5
+            user3.games << game6
+            user3.games << game7
+            user4.games << game7
+            user4.games << game8
+
+            night.users << user
+            night.users << user2
+            night.users << user3
+            night.users << user4
+
+            visit night_path(night)
+
+            page.should have_content(user2.name)
+            page.should have_content(game3.name)
+            page.should have_content(game6.name)
         end
     end
 end
