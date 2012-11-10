2f28651a3afcc2d1e5444759469a09e9c0782caa
John Hutchinson <johnhutch@gmail.com>
2012-11-10 16:20:07 -0500

    spruced up friend add form and messaging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,11 +42,7 @@ class UsersController < ApplicationController
     @users = User.find_by_sql("SELECT * FROM users WHERE email like '%" + params[:email] + "%';")
 
     respond_to do |format|
-      if @users.empty?
-        format.html { redirect_to(dashboard_path, :notice => "No users found.") }
-      else
         format.html { render :action => "search" }
-      end
     end
   end
 
