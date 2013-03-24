2d81a431ce4c0e96ee9101290fa8d6bdab77ac98
John Hutchinson <johnhutch@gmail.com>
2013-03-24 09:34:48 -0400

    refactored night_game_union helper into night model method, night show page formatting

--- app/helpers/nights_helper.rb
+++ app/helpers/nights_helper.rb
@@ -1,10 +1,3 @@
 module NightsHelper
-    def night_game_union(night)
-        game_arr = []
-        for user in night.users do
-            game_arr << user.games
-        end
-        game_arr.inject(game_arr.first) { |f,x| f = f | x }
-    end
 end
 
--- app/models/night.rb
+++ app/models/night.rb
@@ -2,4 +2,12 @@ class Night < ActiveRecord::Base
     attr_accessible :name
     has_and_belongs_to_many :users
     has_many :posts
+
+    def owned_games
+        game_arr = []
+        for user in self.users do
+            game_arr << user.games
+        end
+        game_arr.inject(game_arr.first) { |f,x| f = f | x }
+    end
 end
