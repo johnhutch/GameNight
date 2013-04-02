0ae7e3be556778fab80d9c77d22ef93dfec18e4f
John Hutchinson <johnhutch@gmail.com>
2013-04-01 21:19:09 -0400

    commented non-obvious game_night one liner

--- app/models/night.rb
+++ app/models/night.rb
@@ -8,6 +8,9 @@ class Night < ActiveRecord::Base
         for user in self.users do
             game_arr << user.games
         end
+        # fun little one liner to remove to duplicates
+        # inject steps through the game_arr and does a union with itself
+        # on each element of game_arr
         game_arr.inject(game_arr.first) { |f,x| f = f | x }
     end
 
