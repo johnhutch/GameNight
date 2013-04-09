64b9b4b2f8d74af8381502798f8dddbd802dcafc
Ben Cohen <ben@bencohen.net>
2013-04-09 16:09:42 -0500

    Added roles to seed.rb
    User creation seemed to be failing without the roles existing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+# Create roles
+Role.create([ { name: 'nobody' }, { name: 'commenter' }, { name: 'author' }, { name: 'friend' }, { name: 'uploader' }, { name: 'admin' } ])
